6ad1250e526413333519c12c5b5ff8b517c534b6
Danilo <daniloucelo087@gmail.com>
2024-04-13 22:36:55 -0600

    Desafios

---
+++ Desafio1Calculadora/Desafio1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número:");
+            double num2 = double.Parse(Console.ReadLine());
+            Calculadora_cientifica calc = new Calculadora_cientifica("CASIO", "FX-83GTX");
+            Console.WriteLine("RESULTADO SUMA:");
+            double total = calc.Sumar(num1, num2);
+            Console.WriteLine(total);
+
+            Console.WriteLine("RESULTADO RESTA:");
+            double diferencia = calc.Restar(num1, num2);
+            Console.WriteLine(diferencia);
+
+            Console.WriteLine("RESULTADO MULTIPLICACION:");
+            double producto = calc.Multiplicar(num1, num2);
+            Console.WriteLine(producto);
+
+            Console.WriteLine("RESULTADO DIVICION:");
+            double division = calc.Dividir(num1, num2);
+            Console.WriteLine(division);
+
+
+
+            CalculadoraCientifica cientifica = new CalculadoraCientifica("Casio", "FX-83GTX");
+            Console.WriteLine("RESULTADO RAIZ CUADRADA:");
+            double raiz = cientifica.Raíz(num1);
+            Console.WriteLine(raiz);
+
+            Console.WriteLine("RESULTADO POTENCIA:");
+            double potencia = cientifica.Potencia(num1, num2);
+            Console.WriteLine(potencia);
+
+            Console.WriteLine("RESULTADO MODULO:");
+            double modulo = cientifica.Modulo(num1, num2);
+            Console.WriteLine(modulo);
+
+            Console.WriteLine("RESULTADO LOGARITMO:");
+            double log = cientifica.Logaritmo(num1, num2);
+            Console.WriteLine(log);
+
+            Console.ReadLine();
+
+        }
+    }
+
+    public class Calculadora_cientifica
+    {
+        public string Marca { get; set; }
+        public string Serie { get; set; }
+
+        public Calculadora_cientifica(string marca, string serie)
+        {
+            Marca = marca;
+            Serie = serie;
+        }
+
+        public double Sumar(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public double Restar(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public double Multiplicar(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public double Dividir(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+
+    public class CalculadoraCientifica : Calculadora_cientifica
+    {
+
+        public CalculadoraCientifica(string marca, string serie) : base(marca, serie)
+        {
+        }
+
+        public double Potencia(double num1, double num2)
+        {
+            return Math.Pow(num1, num2);
+        }
+        public double Raíz(double num1)
+        {
+            return Math.Sqrt(num1);
+        }
+        public double Modulo(double n1, double num2)
+        {
+            return n1 % num2;
+        }
+        public double Logaritmo(double num1, double num2)
+        {
+            return Math.Log(num1, num2);
+        }
+    }
+}
---
+++ Desafio_2/Desafio_2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ 
+
+public interface INotificable
+{
+    void Notifica();
+}
+
+public class NotificacionEmail : INotificable
+{
+    public string DireccionCorreo { get; set; }
+
+    public void Notifica()
+    {
+        Console.WriteLine("Enviando notificación por correo electrónico a: {DireccionCorreo}");
+        Console.WriteLine("Hola mundo");
+    }
+}
+
+public class NotificacionWhatsap : INotificable
+{
+    public string NumeroTelefono { get; set; }
+
+    public void Notifica()
+    {
+        Console.WriteLine("Enviando notificación por WhatsApp al número: 34219087");
+        Console.WriteLine("Este es mi programa");
+    }
+}
+
+public class NotificacionSMS : INotificable
+{
+    public string NumeroTelefono { get; set; }
+
+    public void Notifica()
+    {
+        Console.WriteLine($"Enviando notificación por SMS al número: 34219087");
+        Console.WriteLine("Este es mi programa");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var emailNotificacion = new NotificacionEmail { DireccionCorreo = "ucelodani021.com" };
+        var whatsappNotificacion = new NotificacionWhatsap { NumeroTelefono = "34219087" };
+        var smsNotificacion = new NotificacionSMS { NumeroTelefono = "53746026" };
+
+        emailNotificacion.Notifica();
+        whatsappNotificacion.Notifica();
+        smsNotificacion.Notifica();
+        Console.ReadKey();
+    }
+}
